32d6640db3d71882b50554d0c370688d8630860f
SSrirabai <112700014+SSrirabai@users.noreply.github.com>
2023-11-24 12:34:33 +0100

    second commit

--- BlogNVlog/settings.py
+++ BlogNVlog/settings.py
@@ -1,24 +1,17 @@
 # BlogNVlog/settings.py
 
+import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-#4ktqtljjv^8ig$6lvjbpyyn3(r*r84*i1(&wihtgjc5&b8aag'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# Application definition
 INSTALLED_APPS = [
-    # System Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -62,7 +55,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'BlogNVlog.wsgi.application'
 
-# Database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -70,7 +62,6 @@ DATABASES = {
     }
 }
 
-# Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -86,13 +77,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'BlogNVlog' / 'static',
@@ -101,9 +90,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'static_root'
 
-# Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Media files (User-uploaded files like images and videos)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
--- BlogNVlog/urls.py
+++ BlogNVlog/urls.py
@@ -4,11 +4,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from blog.views import post_list
+from blog.views import post_list, RegisterView  # Import your RegisterView here
+from django.contrib.auth.views import LoginView, LogoutView  # Import authentication views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', post_list, name='post_list'),
+    
+    # Authentication views
+    path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    path('register/', RegisterView.as_view(template_name='blog/register.html'), name='register'),
+    
     # Add other paths as needed
 ]
 
